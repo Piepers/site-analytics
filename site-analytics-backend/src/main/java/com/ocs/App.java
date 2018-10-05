cf28366c311acde27cdd4cf311d227952be25511
Bas Piepers <bas@piepers.me>
2018-10-05 18:52:38 +0200

    Project module and gitignore

---
+++ site-analytics-backend/src/main/java/com/ocs/App.java
@@ -0,0 +1,13 @@
+package com.ocs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
