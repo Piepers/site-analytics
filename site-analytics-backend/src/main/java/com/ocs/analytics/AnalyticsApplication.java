64c5436758282d81baa78634293ec5fdfb13032a
Bas Piepers <bas@piepers.me>
2018-10-05 20:03:18 +0200

    Entry point of the application. Package info.

---
+++ site-analytics-backend/src/main/java/com/ocs/analytics/AnalyticsApplication.java
@@ -0,0 +1,15 @@
+package com.ocs.analytics;
+
+import io.vertx.core.Future;
+import io.vertx.reactivex.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnalyticsApplication extends AbstractVerticle {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnalyticsApplication.class);
+
+    @Override
+    public void start(Future<Void> startFuture) {
+        // set the main configuration of our application to the
+    }
+}
---
+++ site-analytics-backend/src/main/java/com/ocs/analytics/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(name = "site-analytics", groupPackage = "com.ocs.analytics")
+package com.ocs.analytics;
+
+import io.vertx.codegen.annotations.ModuleGen;
